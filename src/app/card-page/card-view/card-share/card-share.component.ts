59974d44a8588bcaf6344cc07f5b026396b5f9a0
Paweł Polakiewicz <pawel.polakiewicz1@gmail.com>
2024-06-13 17:00:09 +0200

    Update image source in card-share.component.html

--- src/app/card-page/card-view/card-share/card-share.component.ts
+++ src/app/card-page/card-view/card-share/card-share.component.ts
@@ -1,4 +1,4 @@
-import { Component, EventEmitter, Input, Output } from '@angular/core';
+import { Component, EventEmitter, Output } from '@angular/core';
 import { SafeUrl } from '@angular/platform-browser';
 import { QRCodeModule } from 'angularx-qrcode';
 import { ClipboardService } from 'ngx-clipboard';
