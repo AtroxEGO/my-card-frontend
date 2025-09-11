0242ad420798f025f1cbe6f99c9293766150cad5
Pawel Polakiewicz <pawel.polakiewicz1@gmail.com>
2025-09-11 19:48:58 +0200

    added simple vercel analitycs

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,4 +1,9 @@
-import { ApplicationConfig, importProvidersFrom } from '@angular/core';
+import {
+  APP_INITIALIZER,
+  ApplicationConfig,
+  importProvidersFrom,
+  isDevMode,
+} from '@angular/core';
 import { provideRouter } from '@angular/router';
 import { routes } from './app.routes';
 import {
@@ -9,6 +14,7 @@ import {
 import { authInterceptor } from './shared/interceptors/auth.interceptor';
 import { TranslateLoader, TranslateModule } from '@ngx-translate/core';
 import { TranslateHttpLoader } from '@ngx-translate/http-loader';
+import { inject } from '@vercel/analytics/*';
 
 export function HttpLoaderFactory(http: HttpClient) {
   return new TranslateHttpLoader(http, './assets/locale/', '.json');
@@ -28,5 +34,11 @@ export const appConfig: ApplicationConfig = {
         },
       }),
     ),
+    {
+      provide: APP_INITIALIZER,
+      useFactory: () => {
+        inject({ mode: isDevMode() ? 'development' : 'production' });
+      },
+    },
   ],
 };
