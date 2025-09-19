e3da469d7ee47f9cebfdedeecce3869dbb3ac4e5
Pawel Polakiewicz <pawel.polakiewicz1@gmail.com>
2025-09-19 21:57:36 +0200

    angular 20

--- src/app/app.config.server.ts
+++ src/app/app.config.server.ts
@@ -1,5 +1,5 @@
+import { provideServerRendering, withRoutes } from '@angular/ssr';
 import { mergeApplicationConfig, ApplicationConfig } from '@angular/core';
-import { provideServerRendering } from '@angular/platform-server';
 import { appConfig } from './app.config';
 import { serverRoutes } from './app.routes.server';
 
